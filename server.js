67598bfa1d8c1a47abc29c3a165cd511d1841779
limteractive <limteractive@gmail.com>
2022-06-30 19:59:46 +0900

    server

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.static(__dirname + '/public'));
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-app.listen(4000, () => {
-    console.log('visit http://127.0.0.1:4000');
-});
+// app.listen(4000, () => {
+//     console.log('visit http://127.0.0.1:4000');
+// });
 
-// app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000);
